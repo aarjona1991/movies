652c40e4c149138b8e9438450f11be126105d6aa
aarjona1991 <aarjona1991@gmail.com>
2024-03-31 18:53:24 -0300

    deleting tests

--- src/pages/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Home } from '.';
-
-test('Testing Slides in Slider', () => {
-  render(<Home />);
-  const linkElement = screen.getAllByText('Comprar Ticket');
-  expect(linkElement).toBeInTheDocument();
-});
